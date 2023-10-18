df58ab69c97b0303d657098c59ccfd821a2dbadc
Jason <jasonbohio@gmail.com>
2023-10-18 10:44:40 -0400

    use

--- src/App.js
+++ src/App.js
@@ -104,7 +104,7 @@ function App() {
     getPrice();
     getWalletBalance();
     getTransactions();
-  }, [getPrice]);
+  }, []);
 
   useEffect(() => {
     const priceInterval = setInterval(() => {
